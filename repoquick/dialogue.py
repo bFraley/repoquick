3dfefd098a204368aa5bfc6e0e9df4838a655f78
bFraley <bFraley@users.noreply.github.com>
2016-12-07 22:24:11 -0500

    start resources.py

--- repoquick/dialogue.py
+++ repoquick/dialogue.py
@@ -1,13 +1,7 @@
-commands = ['license', 'ignore']
+# repoquick dialogue strings.
+commands = ['license', 'ignore', 'new', 'help']
 
+# User information settings.
 author = 'repoquick default author not set'
+github_username = 'repoquick github username not set'
 year = '2016'
-
-license_names = {
-    'BSD3':'bsd-3-clause.txt',
-    'MIT':'mit.txt'
-}
-
-ignore_names = {
-    'Python':'Python.gitignore'
-}
--- repoquick/resources/resources.py
+++ repoquick/resources/resources.py
@@ -1,2 +1,10 @@
 from meta_files import ignore_meta, license_meta
         
+spdix_codes_and_filepaths = {
+    'BSD3':'bsd-3-clause.txt',
+    'MIT':'mit.txt'
+}
+
+ignore_paths = {
+    'Python':'Python.gitignore'
+}
